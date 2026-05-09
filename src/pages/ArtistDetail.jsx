edff8fdd0f99b2d282eb698fb838e3879cf7f1f3
Sisouko <ahmedabdrebbi48@gmail.com>
2026-05-09 21:09:01 +0100

    Fix--Probleme-in--ArtistDetail..Page

--- src/pages/ArtistDetail.jsx
+++ src/pages/ArtistDetail.jsx
@@ -1,95 +1,96 @@
-import { useParams, useNavigate } from 'rect-router-dom'
-import { useFestival } from '../context/FestivalContext'
+// import { useParams, useNavigate } from 'react-router-dom'
+// import { useFestival } from '../context/FestivalContext'
 
 
-function ArtistDetail() {
+// function ArtistDetail() {
 
-    const { artistId } = useParam()
-    const navigate = useNavigate()
-    const { toggleFavorite, isFavorite, markAttended, isAttended } = useFestival()
+//     const { artistId } = useParam()
+//     const navigate = useNavigate()
+//     const { toggleFavorite, isFavorite, markAttended, isAttended } = useFestival()
 
-    const artist = artist.find(a => a.id === artistId)
-    if (!artist) {
-        return (
-<div className = "error-page">
-    <p>Artiste not Fouuuuuuuuuuund ;) <p/> 
-    <button onClick= {() => navigate ('/programe')}>
-        ← Go back to Programe
-        <button/>
-<div/>
-        )
-    }
+//     const artist = artist.find(a => a.id === artistId)
+//     if (!artist) {
+//         return (
 
-const favorited = isFavorite(artist.id)
-const attended = isAttended(artist.id)
+// <div className = "error-page">
+//     <p>Artiste not Fouuuuuuuuuuund ;) <p/> 
+//     <button onClick= {() => navigate ('/programe')}>
+//         ← Go back to Programe
+//         <button/>
+// <div/>
+//         )
+//     }
 
-function handleAttended() {
+// const favorited = isFavorite(artist.id)
+// const attended = isAttended(artist.id)
 
-marAttended(artist)
-navigate('/passeport')
+// function handleAttended() {
 
-}
-return (
+// marAttended(artist)
+// navigate('/passeport')
 
-    <div className="artist-detail">
-        <button className="bach-btn" onClick={() => navigate('/programe')}>
-            ← Programme
-            <button/>
-     <div className="artist-details__header">
-        <div className="artist-detail__avatar">
-            {artist.name.charAt(0)}
-            <div/>
-            <h1 className="artist-detail__name">{artist.name}<h1/>
-            <span className="artist-detail__genre">{artist.genre}<span/>
-        <div/>       
-<div className="artist-detail__info">
-        <div className="info-item">
-          <span className="info-item__icon">📍</span>
-          <span className="info-item__value">{artist.stage}</span>
-        </div>
-        <div className="info-item">
-          <span className="info-item__icon">🕐</span>
-          <span className="info-item__value">{artist.time}</span>
-        </div>
-        <div className="info-item">
-          <span className="info-item__icon">📅</span>
-          <span className="info-item__value">
-            {new Date(artist.day).toLocaleDateString('fr-FR', {
-              weekday: 'long',
-              day: 'numeric',
-              month: 'long'
-            })}
-          </span>
-        </div>
-      </div>
+// }
+// return (
 
-<div className="artist-detail__bio">
-        <h2>Biographie</h2>
-        <p>{artist.bio}</p>
-      </div>
+//     <div className="artist-detail">
+//         <button className="bach-btn" onClick={() => navigate('/programe')}>
+//             ← Programme
+//             <button/>
+//      <div className="artist-detailsheader">
+//         <div className="artist-detailavatar">
+//             {artist.name.charAt(0)}
+//             <div/>
+//             <h1 className="artist-detailname">{artist.name}<h1/>
+//             <span className="artist-detailgenre">{artist.genre}<span/>
+//         <div/>       
+// <div className="artist-detailinfo">
+//         <div className="info-item">
+//           <span className="info-itemicon">📍</span>
+//           <span className="info-itemvalue">{artist.stage}</span>
+//         </div>
+//         <div className="info-item">
+//           <span className="info-itemicon">🕐</span>
+//           <span className="info-itemvalue">{artist.time}</span>
+//         </div>
+//         <div className="info-item">
+//           <span className="info-itemicon">📅</span>
+//           <span className="info-itemvalue">
+//             {new Date(artist.day).toLocaleDateString('fr-FR', {
+//               weekday: 'long',
+//               day: 'numeric',
+//               month: 'long'
+//             })}
+//           </span>
+//         </div>
+//       </div>
 
-
-<div className="artist-detail__actions">
-        <button
-          className={`btn ${favorited ? 'btn--favorited' : 'btn--outline'}`}
-          onClick={() => toggleFavorite(artist)}
-        >
-          {favorited ? '❤️ Dans mon Planning' : '🤍 Ajouter au Planning'}
-        </button>
-
-        <button
-          className={`btn ${attended ? 'btn--attended' : 'btn--primary'}`}
-          onClick={handleAttended}
-          disabled={attended}
-        >
-          {attended ? '✓ J\'y étais' : '✓ Marquer J\'y étais'}
-        </button>
-      </div>
-
-    </div>
+// <div className="artist-detailbio">
+//         <h2>Biographie</h2>
+//         <p>{artist.bio}</p>
+//       </div>
 
 
-)
+// <div className="artist-detail__actions">
+//         <button
+//           className={btn ${favorited ? 'btn--favorited' : 'btn--outline'}}
+//           onClick={() => toggleFavorite(artist)}
+//         >
+//           {favorited ? '❤️ Dans mon Planning' : '🤍 Ajouter au Planning'}
+//         </button>
 
-}export function default ArtistDetail
+//         <button
+//           className={btn ${attended ? 'btn--attended' : 'btn--primary'}}
+//           onClick={handleAttended}
+//           disabled={attended}
+//         >
+//           {attended ? '✓ J\'y étais' : '✓ Marquer J\'y étais'}
+//         </button>
+//       </div>
+
+//     </div>
+
+// )
+
+// }
+// }
 
