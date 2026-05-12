81eb721a45103e44fd10964d94075fa956ad86a2
Sisouko <ahmedabdrebbi48@gmail.com>
2026-05-12 22:31:52 +0100

    Edit--Context__CODE

--- src/context/FestivalContext.jsx
+++ src/context/FestivalContext.jsx
@@ -6,14 +6,8 @@ const FestivalContext = createContext ()
 
 export function FestivalProvider({children}){
 
-    const [favorites, setFavorites] = useState(() => {
-        const saved = localStorage.getItem('beat-favorites');
-        return saved ? JSON.parse(saved) : [];
-    })
-    const [passeport, setPasseport] = useState(() => {
-        const saved = localStorage.getItem('beat-passeport');
-        return saved ? JSON.parse(saved) : [];
-    })
+    const [favorites, setFavorites] = useState([])
+    const [passeport, setPasseport] = useState([])
 
 useEffect (() => {
     localStorage.setItem('beat-favorites', JSON.stringify(favorites))
